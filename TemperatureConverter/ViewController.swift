da1ed06f60f52b36c39f847ad15fbfc588fb3548
Tonny <tonny@Tonnys-Air.lan>
2014-11-30 20:58:45 +0800

    Part II finished

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    func updateTemperatureDisplays(){
+        // Define
+        var C = self.temperatureSlider.value
+        var F = (C * 9/5) + 32
+        var K = C + 273.15
+        // Change var
+        self.celsiusLabel.text = String(format: "%.1f", C) + "°C"
+        self.fahrenheitLabel.text = String(format: "%.1f", F) + "°F"
+        self.kelvinLabel.text = String(format: "%.1f", K) + "K"
+    }
+    @IBAction func temperatureSliderChanged(sender: UISlider) {
+        // TODO: read the current value of the slider and update the temperature displays
+        self.updateTemperatureDisplays()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +41,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        let screen = UIScreen.mainScreen()
+//        // Always return the size of the screen in portraits orientation
+//        println("screen fixed bounds: \(screen.fixedCoordinateSpace.bounds)")
+//        // Since ios8, this is the size of the screen in its current orientation
+//        println("screen bounds: \(screen.bounds)")
+//        println("top layout guide: \(topLayoutGuide.length)")
+//        // TODO: Calculate the title label's frame.
+//        titleLabel.frame = CGRect(x: 16.0,y: topLayoutGuide.length + 16.0,width: screen.bounds.width-32,height: 21.0)
+//    }
 
 }
 
